0946e0ee029ec1730e4fd4af52dce03ac17e0d2f
Mauricio Hernan Cabrera <mauriciohernancabrera@gmail.com>
2019-01-17 13:08:56 -0300

    14. ¿Cómo diseñar URLs?

--- components/Layout.js
+++ components/Layout.js
@@ -11,7 +11,7 @@ export default class Layout extends Component {
         <Head>
           <title>{ title }</title>
           <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-          <meta charset="UTF-8"/>
+          <meta charSet="UTF-8"/>
         </Head>
         
         <header>
